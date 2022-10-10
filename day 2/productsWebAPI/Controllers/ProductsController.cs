3ce9e3a5f0045f472f0beae6421d7fd167ee37a7
Nikhil <nikhil.shah2008@gmail.com>
2022-10-10 12:55:30 +0530

    Day 2

---
+++ day 2/productsWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using productsWebAPI.Model;
+namespace productsWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Model Obj
+        //We should use Dependency injection instead of creating the new object 
+        //this is a crime
+        Products pObj = new Products();
+
+
+        [HttpGet]
+        [Route("plist")]
+        public IActionResult GetallProducts()
+        {
+            return Ok(pObj.GetALlProducts());
+        }
+
+        [HttpGet]
+        [Route("plist/{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            try
+            {
+                return Ok(pObj.GetProductById(id));
+            }
+            catch (Exception es)
+            {
+
+                return NotFound(es.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("plist/add")]
+        public IActionResult AddNewProduct(Products newProduct)
+        {
+            return Created("",pObj.AddProduct(newProduct));
+        }
+
+        [HttpDelete]
+        [Route("plist/{id}")]
+         public IActionResult DeleteProduct(int id)
+        {
+            try
+            {
+                return Accepted(pObj.DeleteProduct(id));
+            }
+            catch (Exception es)
+            {
+
+                return NotFound(es.Message);
+
+            }
+        }
+
+        [HttpPut]
+        [Route("plist/update")]
+        public IActionResult UpdateProduct(Products updates)
+        {
+            try
+            {
+                return  Ok(pObj.UpdateProduct(updates));
+            }
+            catch (Exception es)
+            {
+
+                return NotFound(es.Message);
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ day 2/productsWebAPI/Model/Products.cs
@@ -0,0 +1,92 @@
+﻿namespace productsWebAPI.Model
+{
+    public class Products
+    {
+
+        #region Properties
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public double pPrice { get; set; }
+        public bool pIsInStock { get; set; }
+
+        #endregion
+
+        #region Data
+
+        static List<Products> pList = new List<Products>()
+        {
+            new Products(){ pId=101, pName="Pepsi", pCategory="Cold-Drink", pIsInStock=true, pPrice=50},
+            new Products(){ pId=102, pName="Maggie", pCategory="Fast-Food", pIsInStock=true, pPrice=50},
+            new Products(){ pId=103, pName="IPhone", pCategory="Electronic", pIsInStock=false, pPrice=50},
+            new Products(){ pId=104, pName="Flying Machine", pCategory="Jeans", pIsInStock=true, pPrice=50},
+            new Products(){ pId=105, pName="Green-tea", pCategory="Health-Drink", pIsInStock=false, pPrice=50},
+        };
+
+
+        #endregion
+
+        #region Get Methods
+        public List<Products> GetALlProducts()
+        {
+            return pList;
+        }
+
+        public Products GetProductById(int id)
+        {
+            var p = pList.Find(pr => pr.pId == id);
+            if (p != null)
+            {
+                return p;
+            }
+            throw new Exception("Product Not Found in System");
+        }
+        #endregion
+
+        #region Add, Update and Delete
+        public string AddProduct(Products newProduct)
+        {
+            //perform some validation
+
+            pList.Add(newProduct);
+            return "Product Added Successfully";
+        }
+
+        public string DeleteProduct(int pid)
+        {
+            var p = pList.Find(pr => pr.pId == pid);
+            if (p != null)
+            {
+                pList.Remove(p);
+                return "Product Deleted Successfully";
+            }
+            throw new Exception("Product Not found - and thus cannot be deleted");
+        }
+
+        public string UpdateProduct(Products product)
+        {
+            var p = pList.Find(pr => pr.pId == product.pId);
+            if (p != null)
+            {
+                p.pName = product.pName;
+                p.pCategory = product.pCategory;
+                p.pPrice = product.pPrice;
+                p.pIsInStock = product.pIsInStock;
+                return "Product updated successfully";
+            }
+            throw new Exception("Product Not found - and thus cannot be Edited");
+        }
+        #endregion
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
